c3b1469da8e037ed1ec995dcf1bda2b11512f362
nanafan93 <limayehnl2712@gmail.com>
2025-10-01 16:19:39 +1000

    Parse midi header. Try variants of the reading APi

--- main.go
+++ main.go
@@ -1,29 +1,71 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+type MIDIHeader struct {
+	ChunkType [4]byte // "MThd"
+	Length    uint32  // always 6
+	Format    uint16
+	NumTracks uint16
+	Division  uint16
+}
+
+func printEntireFile(file *os.File) {
+	buf := make([]byte, 512)
+	totalSize := 0
+	printedBytes := 0
+	for {
+		n, err := file.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Fatal("something went wrong reading the file")
+			}
+		}
+		totalSize += n
+		for _, fileByte := range buf[:n] {
+			fmt.Printf("%02X ", fileByte)
+			printedBytes++
+			if (printedBytes+1)%10 == 0 {
+				fmt.Println()
+			}
+		}
+	}
+	fmt.Printf("\nMIDI size: %d bytes\n", totalSize)
+}
 
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: midi <midi-file>")
 		os.Exit(1)
 	}
+	header := make([]byte, 14)
 	fileName := os.Args[1]
-	fileBytes, err := os.ReadFile(fileName)
+	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal("Error reading file: ", err)
 	}
-	fmt.Printf("MIDI size: %d bytes\n", len(fileBytes))
-	for i, fileByte := range fileBytes {
-		fmt.Printf("%02X ", fileByte)
-		if (i+1)%10 == 0 {
-			fmt.Println()
-		}
+	printEntireFile(file)
+	defer file.Close()
+	file.Seek(0, io.SeekStart)
+	n, err := file.Read(header)
+	if err != nil || n < 14 {
+		log.Fatal("Cannot read header: ", err)
 	}
+	if string(header[:4]) != "MThd" {
+		log.Fatal("Not a valid MIDI file (missing MThd header)")
+	}
+	length := binary.BigEndian.Uint32(header[4:8])
+	format := binary.BigEndian.Uint16(header[8:10])
+	nTracks := binary.BigEndian.Uint16(header[10:12])
+	division := binary.BigEndian.Uint16(header[12:14])
+	fmt.Printf("Header length: %d\nFormat: %d\nTracks: %d\nDivision: %d\n", length, format, nTracks, division)
+
 }
